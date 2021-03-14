2d258e4e21e3b22ee6a31a0fcf9697d097a59aa8
taisei sumiyama <r70s4effort@gmail.com>
2021-03-14 23:38:41 +0900

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by taisei sumiyama on 2021/03/14.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

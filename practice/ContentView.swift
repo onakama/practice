e24de81e5796b295aa1b94102ce88a469e1de68f
sumi <ninzin60en.dev@gmail.com>
2021-03-21 14:45:19 +0900

    pikkar

--- practice/ContentView.swift
+++ practice/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State var R:Double = 0
     @State var G:Double = 0
     @State var B:Double = 0
+    @State private var selectedColor = 0
+    let colorViews = [Color.red, Color.green, Color.blue]
+    let colorNames = ["Red", "Green", "Blue"]
     
     var body: some View {
         VStack{
@@ -24,84 +27,103 @@ struct ContentView: View {
             ///            print(num)
                 }) {
                     Text("Ramdom Button")
-                        .background(Color.gray)
+                    .background(Color.gray)
                 }
 
             
                 Text("\(num)")
-                    .font(.largeTitle)
+                .font(.largeTitle)
                     
                 Stepper(value: $num, in: 0 ... 100) {
                 }
-                    .padding(0)
-                    .frame(width : 90)
+                .padding(0)
+                .frame(width : 90)
             }
-            ///回転ボタン
-            Button(action: {
-                withAnimation{
-                    self.isFast.toggle()
-                }
-            }) {
-                Image(systemName: isFast ? "hare" : "tortoise")
-                    .rotationEffect(.degrees(isFast ? 0 : 180))
-                    .scaleEffect(3)
-            }
-                .padding(10)
             
             ///トグル
             HStack{
                 Toggle(isOn: $iLike) {
                     Text("Like or Not")
-                        .font(.largeTitle)
+                    .font(.largeTitle)
                 }
-                    .fixedSize()
-                    .padding(10)
+                .fixedSize()
+                .padding(10)
                 
                 if iLike{
                     Text("Like")
-                        .foregroundColor(.red)
-                        .font(.largeTitle)
+                    .foregroundColor(.red)
+                    .font(.largeTitle)
                 }else{
                     Text("Not")
-                        .foregroundColor(.green)
-                        .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .font(.largeTitle)
                 }
             }
-            
-            ///スライダー
-            Circle()
-                .frame(width: 70,height: 70)
+            HStack{
+                ///回転ボタン
+                Button(action: {
+                    withAnimation{
+                        self.isFast.toggle()
+                    }
+                }) {
+                    Image(systemName: isFast ? "hare" : "tortoise")
+                    .rotationEffect(.degrees(isFast ? 0 : 180))
+                    .scaleEffect(3)
+                }
+                .padding(10)
+//                ///スライダー
+//                Circle()
+//                .frame(width: 70,height: 70)
+//                .padding()
+//                .foregroundColor(Color(red: R/255, green: G/255, blue: B/255))
+                }
+//            HStack{
+//                Circle()
+//                .foregroundColor(.red)
+//                .frame(width: 20, height: 20)
+//                Text(String(Int(R)))
+//                .frame(width: 40)
+//                Slider(value: $R, in: 0 ... 255)
+//                .frame(width: 200)
+//            }
+//            HStack{
+//                Circle()
+//                .foregroundColor(.green)
+//                .frame(width: 20, height: 20)
+//                Text(String(Int(G)))
+//                .frame(width: 40)
+//                Slider(value: $G, in: 0 ... 255)
+//                .frame(width: 200)
+//            }
+//            HStack{
+//                Circle()
+//                .foregroundColor(.blue)
+//                .frame(width: 20, height: 20)
+//                Text(String(Int(B)))
+//                .frame(width: 40)
+//                Slider(value: $B, in: 0 ... 255)
+//                .frame(width: 200)
+//            }
+            VStack{
+                Picker(selection: $selectedColor, label: Text("Color")) {
+                    Text("Red").tag(0)
+                    Text("Grren").tag(1)
+                    Text("Blue").tag(2)
+                }
                 .padding()
-                .foregroundColor(Color(red: R/255, green: G/255, blue: B/255))
-            
-            HStack{
-                Circle()
-                    .foregroundColor(.red)
-                    .frame(width: 20, height: 20)
-                Text(String(Int(R)))
-                    .frame(width: 40)
-                Slider(value: $R, in: 0 ... 255)
-                    .frame(width: 200)
+                .frame(height: 100)
+                
+                HStack{
+                    colorViews[selectedColor]
+                        .frame(width: 50, height: 100)
+                    Text("value: \(selectedColor)")
+                        .frame(width: 60)
+                    Text("\(colorNames[selectedColor])")
+                        .frame(width: 70)
+                    
+                }
+                
             }
-            HStack{
-                Circle()
-                    .foregroundColor(.green)
-                    .frame(width: 20, height: 20)
-                Text(String(Int(G)))
-                    .frame(width: 40)
-                Slider(value: $G, in: 0 ... 255)
-                    .frame(width: 200)
-            }
-            HStack{
-                Circle()
-                    .foregroundColor(.blue)
-                    .frame(width: 20, height: 20)
-                Text(String(Int(B)))
-                    .frame(width: 40)
-                Slider(value: $B, in: 0 ... 255)
-                    .frame(width: 200)
-            }
-            
         }
     }
 }
